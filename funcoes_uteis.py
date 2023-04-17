3d448e827aed2222e890dfca250781992760ee3f
reisarthur2 <reisarthur2@gmail.com>
2023-04-17 20:24:21 -0300

    correções de nomes e adicionado funcao de dois gráficos

--- funcoes_uteis.py
+++ funcoes_uteis.py
@@ -1,21 +1,57 @@
 from numpy import linspace,trapz
 import matplotlib.pyplot as mp
+from typing import Callable,Iterable
+
 #funcoes basicas para fazer analise
-def integral (funcao,lim_sup:float=1,lim_inf:float=0):
-    precisao_por_unidade=100
+def integral (funcao: Callable,lim_sup:float=1,lim_inf:float=0):
+    precisao_por_unidade=250
     intervalo=linspace(lim_inf,lim_sup,precisao_por_unidade)
     return trapz(funcao(intervalo),intervalo)
 
-def derivada (funcao,x):
+def derivada (funcao:Callable,x:float) -> float:
     return (funcao(x+0.0001)-funcao(x))/0.0001
 
-
-def plotar (funcao1,funcao2,funcao3):
-    x=linspace(0,1,100)
+def plotar (*args:Callable,**kwargs):
+    numero_amostras= kwargs.get ('numero_amostras',100)
+    escalax=kwargs.get ('escala_x',0.1)
+    escalay=kwargs.get ('escala_y',0.1)
+    linha_espessura=kwargs.get ('linha_espessura',0.5)
+    x=linspace(0,1,numero_amostras)
     fig, eixos = mp.subplots(layout='constrained')
-    eixos.plot (x,tuple(map(funcao1,x)),scalex=0.1,scaley=.01,linewidth=.5,label='ue1')
-    eixos.plot (x,tuple(map(funcao2,x)),scalex=0.1,scaley=.01,linewidth=.5,label='ue2')
-    eixos.plot (x,tuple(map(funcao3,x)),scalex=0.1,scaley=.01,linewidth=.5,label='u0')
+    for funcao in args:
+        eixos.plot (x,tuple(map(funcao,x)),scalex=escalax,scaley=escalay,linewidth=linha_espessura,label=funcao.__name__)
     eixos.grid()
     eixos.legend()
+    mp.show()
+
+def grafico_duplo (*args:Iterable,**kwargs):
+    numero_amostras= kwargs.get ('numero_amostras',100)
+    escalax=kwargs.get ('escala_x',0.1)
+    escalay=kwargs.get ('escala_y',0.1)
+    linha_espessura=kwargs.get ('linha_espessura',0.5)
+    titulo1=kwargs.get ('titulo_1','sem nada')
+    titulo2=kwargs.get ('titulo_2','sem nada')
+    eixox1=kwargs.get ('eixo_x1','x')
+    eixox2=kwargs.get ('eixo_x1','x')
+    eixoy1=kwargs.get ('eixo_y1',r'Aproximações de $U^{ε}$')
+    eixoy2=kwargs.get ('eixo_y2',r'$N_{x/ε}$')
+    
+    x=linspace(0,1,numero_amostras)
+    fig, (eixo1,eixo2) = mp.subplots(ncols=2,layout='constrained')
+    for funcao in args[0]:
+        eixo1.plot (x,tuple(map(funcao,x)),scalex=escalax,scaley=escalay,linewidth=linha_espessura,label=funcao.__name__)
+    for funcao in args[1]:
+        eixo2.plot (x,tuple(map(funcao,x)),scalex=escalax,scaley=escalay,linewidth=linha_espessura,label=funcao.__name__)
+    eixo1.grid()
+    eixo1.legend()
+    eixo1.set_title(f"{titulo1}",size=20)
+    eixo1.set_xlabel (eixox1)
+    eixo1.set_ylabel (eixoy1,size=15)
+    
+    eixo2.grid()
+    eixo2.legend()
+    eixo2.set_title(f"{titulo2}",size=20)
+    eixo2.set_xlabel (eixox2)
+    eixo2.set_ylabel (eixoy2,size=15)
+    
     mp.show()
